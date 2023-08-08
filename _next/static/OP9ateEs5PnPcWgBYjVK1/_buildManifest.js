42b8a72de1494f63f4c55f403dcade0ae6da214c
nekevss <nekevss@users.noreply.github.com>
2023-08-08 02:44:14 +0000

    deploy: 307474d234d83635aa84aeac7360009673fb3119

---
+++ _next/static/OP9ateEs5PnPcWgBYjVK1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f947972f8c1f41fc.css","static/chunks/pages/index-cbbcaa7e0a7f6384.js"],"/_error":["static/chunks/pages/_error-9db8cfb73c3ed58d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
